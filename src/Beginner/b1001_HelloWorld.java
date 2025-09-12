861aba7698c14ab7b354dc642ce3a47884e79409
Breno Ferraz <Ferrazbreno42@outlook.com>
2025-09-12 11:20:00 -0300

    Criação do projeto e primeiro problema resolvido

--- src/Beginner/b1001_HelloWorld.java
+++ src/Beginner/b1001_HelloWorld.java
@@ -2,7 +2,7 @@ package Beginner;
 
 public class b1001_HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
 
     }
 }
